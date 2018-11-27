3aeac8aef69e7e6ebb4ad3ef68c5ac29c1b983c2
davis opondo <opondodav201994@gmail.com>
2018-11-27 09:31:44 +0300

    add parseInt functions,prompt functions and return function

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-var add = function(number1, number2) {
-    return number1 + number2;
+var add=function(number0, number1, number2, number3, number4, number5,number6,number7,number8,number9){
+  return number0 + number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9;
   };
-  var number1 = prompt("Enter a number:");
-  alert(number1);
+  var number0=parseInt(prompt("enter a number:"));
+  var number1=parseInt(prompt("enter another number:"));
+  var number2=parseInt(prompt("enter another number:"));
+  var number3=parseInt(prompt("enter another number:"));
+  var number4=parseInt(prompt("enter another number:"));
+  var number5=parseInt(prompt("enter another number:"));
+  var number6=parseInt(prompt("enter another number:"));
+  var number7=parseInt(prompt("enter another number:"));
+  var number8=parseInt(prompt("enter another number:"));
+  var number9=parseInt(prompt("enter another number:"));
+  var result=add(number0, number1,number2,number3,number4,number5,number6,number7,number8,number9);
+  alert(result);
+  var subtract=function(number1, number2){
+  return number1 - number2;
+  };
+  alert (subtract(10,5));
